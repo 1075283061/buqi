0956315ab58fd708c42aac36d0e53490bd558559
1075283061 <1075283061@qq.com>
2017-08-31 22:20:01 +0800

    buqi

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
 <footer>
  <p>222222222222</p>
 </footer>
+</body>
+</html>  
--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>布奇纺织</title>
-</head>
-<body>
-<?php
-  require_once "./header.php";
-?>
-
-h
-
-<?php
-  require_once "./footer.php";
-?>
-</body>
-</html>
+<?php require_once "./head.php";?>
+<?php require_once "./header.php";?>
+<?php require_once "./footer.php";?>
