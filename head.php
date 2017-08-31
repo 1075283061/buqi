1a7a555284aebe6cc4fa9a7791add3b768e84199
1075283061 <1075283061@qq.com>
2017-08-31 23:32:46 +0800

    buqi

--- head.php
+++ head.php
@@ -5,5 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>布奇纺织</title>
+  <link href="https://cdn.bootcss.com/normalize/7.0.0/normalize.css" rel="stylesheet">
 </head>
 <body>
--- header.php
+++ header.php
@@ -1,3 +1,63 @@
-<header>
-  <p>11111111111</p>
+<header class="heaher">
+  <div class="header_div">
+    <a class="logo" href=""><img src="./logo.png" alt="LOGO"></a>
+    <nav class="header_nav">
+      <ul class="header_ul">
+        <li class="header_li">
+            <a class="header_a" href="./index.php">
+            <p class="cn_p">布奇首页</p>
+            <p class="en_p">home</p>
+            </a>   
+        </li>
+        <li class="header_li">
+            <a class="header_a1" href="">
+            <p class="cn_p">走进布奇</p>
+            <p class="en_p">Into the Bq</p>
+            </a>   
+        </li>
+        <li class="header_li">
+            <a class="header_a1" href="">
+            <p class="cn_p">关于布奇</p>
+            <p class="en_p">about bq</p>
+            </a>   
+        </li>
+        <li class="header_li">
+            <a class="header_a1" href="">
+            <p class="cn_p">产品中心</p>
+            <p class="en_p">products</p>
+            </a>   
+        </li>
+        <li class="header_li">
+            <a class="header_a1" href="">
+            <p class="cn_p">案例分享</p>
+            <p class="en_p">Case sharing</p>
+            </a>   
+        </li>
+        <li class="header_li">
+            <a class="header_a1" href="">
+            <p class="cn_p">展会现场</p>
+            <p class="en_p">Exhibition</p>
+            </a>   
+        </li>
+        <li class="header_li">
+            <a class="header_a1" href="">
+            <p class="cn_p">我的diy</p>
+            <p class="en_p">my diy</p>
+            </a>   
+        </li>
+        <li class="header_li">
+            <a class="header_a1" href="">
+            <p class="cn_p">设计师中心</p>
+            <p class="en_p">Designer</p>
+            </a>   
+        </li>
+        <li class="header_li">
+            <a class="header_a1" href="">
+            <p class="cn_p">联系我们</p>
+            <p class="en_p">contact</p>
+            </a>   
+        </li>
+      </ul>
+    </nav>
+  </div>
 </header>
