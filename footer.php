a894470918399c11dd5c86384754219e742b953e
1075283061 <1075283061@qq.com>
2017-09-02 01:37:56 +0800

    buqi

--- footer.php
+++ footer.php
@@ -1,4 +1,86 @@
-<footer>
+<footer class="tem_footer">
+  <nav class="footer_nav">
+    <div class="tem_div">
+      <a href="./index.php" class="home">home</a>
+      <div class="footer_main">
+        <div class="main_lf">
+          <div class="main_table">
+            <li class="footer_li">
+              <dl>
+                <dt>
+                  <a href="">关于布奇</a>
+                </dt>
+                <dd>
+                  <a href="">品牌故事</a>
+                  <a href="">品牌文化</a>
+                  <a href="">品牌优势</a>
+                  <a href="">设计团队</a>
+                  <a href="">品牌DNA</a>
+                  <a href="">智能地图</a>
+                </dd>
+              </dl>
+            </li>
+            <div class="slash">/</div>
+            <li class="footer_li">
+              <dl>
+                <dt>
+                  <a href="">产品中心</a>
+                </dt>
+                <dd>
+                  <a href="">简欧风格</a>
+                  <a href="">中式风格</a>
+                  <a href="">轻奢风格</a>
+                  <a href="">现代风格</a>
+                  <a href="">法式风格</a>
+                  <a href="">美式风格</a>
+                  <a href="">单色</a>
+                </dd>
+              </dl>
+            </li>
+            <div class="slash">/</div>
+            <li class="footer_li">
+              <dl>
+                <dt>
+                  <a href="">我的DIY</a>
+                </dt>
+                <dd>
+                </dd>
+              </dl>
+            </li>
+            <div class="slash">/</div>
+            <li class="footer_li">
+              <dl>
+                <dt>
+                  <a href="">会员中心</a>
+                </dt>
+                <dd>
+                </dd>
+              </dl>
+            </li>
+          </div>
+        </div>
+        <div class="main_rt">
+          <a href="">w</a>
+          <a href="">q</a>
+          <a href="">x</a>
+          <p>|</p>
+          <a href="#">&#8593;</a>
+        </div>
+      </div>
+    </div>        
+  </nav>
+  <div class="footer_bm">
+    <div class="tem_div">
+      <div class="footer_table">
+        <div class="footer_text">
+          Copyright © 2017-2020 海宁布奇纺织有限公司 All Right Reserved  浙ICP备0123456号
+        </div>
+        <div class="footer_nav_text">
+          <a href="http://www.Goomay.cn">技术支持：谷美科技</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
 </body>
 </html>  
--- header.php
+++ header.php
@@ -1,67 +1,71 @@
-<header class="heaher">
+<header class="tem_header">
+<div class="tem_div">
     <div class="header_div">
-        <a class="logo" href=""><img src="./logo.png" alt="LOGO"></a>
-        <nav class="header_nav">
-            <ul class="header_ul">
-                <li class="header_li">
-                    <a class="header_a" href="./index.php">
-                    <p class="cn_p">布奇首页</p>
-                    <p class="en_p">home</p>
-                    </a>   
-                </li>
-                <li class="header_li">
-                    <a class="header_a" href="">
-                    <p class="cn_p">走进布奇</p>
-                    <p class="en_p">Into the Bq</p>
-                    </a>   
-                </li>
-                <li class="header_li">
-                    <a class="header_a" href="">
-                    <p class="cn_p">关于布奇</p>
-                    <p class="en_p">about bq</p>
-                    </a>   
-                </li>
-                <li class="header_li">
-                    <a class="header_a" href="">
-                    <p class="cn_p">产品中心</p>
-                    <p class="en_p">products</p>
-                    </a>   
-                </li>
-                <li class="header_li">
-                    <a class="header_a" href="">
-                    <p class="cn_p">案例分享</p>
-                    <p class="en_p">Case sharing</p>
-                    </a>   
-                </li>
-                <li class="header_li">
-                    <a class="header_a" href="">
-                    <p class="cn_p">展会现场</p>
-                    <p class="en_p">Exhibition</p>
-                    </a>   
-                </li>
-                <li class="header_li">
-                    <a class="header_a" href="">
-                    <p class="cn_p">我的diy</p>
-                    <p class="en_p">my diy</p>
-                    </a>   
-                </li>
-                <li class="header_li">
-                    <a class="header_a" href="">
-                    <p class="cn_p">设计师中心</p>
-                    <p class="en_p">Designer</p>
-                    </a>   
-                </li>
-                <li class="header_li">
-                    <a class="header_a" href="">
-                    <p class="cn_p">联系我们</p>
-                    <p class="en_p">contact</p>
-                    </a>   
-                </li>
-                <li class="header_li">
-                    <a href="" class="language">中</a>
-                    <a href="" class="language">EN</a>
-                </li>
-            </ul>
-        </nav>
+        <a class="logo" href=""><img src="./img/logo.png" alt="LOGO"></a>
+        <div class="header_rt">
+            <nav class="header_nav">
+                <ul class="header_ul">
+                    <li class="header_li">
+                        <a class="header_a" href="./index.php">
+                        <p class="cn_p">布奇首页</p>
+                        <p class="en_p">home</p>
+                        </a>   
+                    </li>
+                    <li class="header_li">
+                        <a class="header_a" href="">
+                        <p class="cn_p">走进布奇</p>
+                        <p class="en_p">Into the Bq</p>
+                        </a>   
+                    </li>
+                    <li class="header_li">
+                        <a class="header_a" href="">
+                        <p class="cn_p">关于布奇</p>
+                        <p class="en_p">about bq</p>
+                        </a>   
+                    </li>
+                    <li class="header_li">
+                        <a class="header_a" href="">
+                        <p class="cn_p">产品中心</p>
+                        <p class="en_p">products</p>
+                        </a>   
+                    </li>
+                    <li class="header_li">
+                        <a class="header_a" href="">
+                        <p class="cn_p">案例分享</p>
+                        <p class="en_p">Case sharing</p>
+                        </a>   
+                    </li>
+                    <li class="header_li">
+                        <a class="header_a" href="">
+                        <p class="cn_p">展会现场</p>
+                        <p class="en_p">Exhibition</p>
+                        </a>   
+                    </li>
+                    <li class="header_li">
+                        <a class="header_a" href="">
+                        <p class="cn_p">我的diy</p>
+                        <p class="en_p">my diy</p>
+                        </a>   
+                    </li>
+                    <li class="header_li">
+                        <a class="header_a" href="">
+                        <p class="cn_p">设计师中心</p>
+                        <p class="en_p">Designer</p>
+                        </a>   
+                    </li>
+                    <li class="header_li">
+                        <a class="header_a" href="">
+                        <p class="cn_p">联系我们</p>
+                        <p class="en_p">contact</p>
+                        </a>   
+                    </li>
+                </ul>
+            </nav>
+            <div class="header_language">
+                <a href="" class="language">中</a>
+                <a href="" class="language">EN</a>
+            </div>
+        </div>
     </div>
+</div>
 </header>
