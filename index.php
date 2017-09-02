786ab54847c366791c32a836c65ba80144132de3
1075283061 <1075283061@qq.com>
2017-09-02 15:56:26 +0800

    buqi

--- index.php
+++ index.php
@@ -1,3 +1,61 @@
 <?php require_once "./head.php";?>
-<?php require_once "./header.php";?>
-<?php require_once "./footer.php";?>
+    <!-- banner -->
+    <div class="index_banner">
+      <img src="./img/index_banner1.jpg" alt="index_banner">
+    </div>
+    <!-- index_logo -->
+    <div class="index_logo">
+      <div class="top_table">
+        <div class="top_logo">
+        <img src="./img/index_logo.png" alt="logo">
+        </div>
+      </div>
+    </div>
+    <div class="index_body">
+      <?php require_once "./header.php";?>
+      <!-- index_brand -->
+      <section class="index_brand">
+        <div class="tem_div">
+          <div class="brand_content">
+            <div class="brand_title">
+              <h2 class="title1">
+                <p>
+                品牌简介
+                </p>
+                <span>Brand profile</span>
+              </h2>
+            </div>
+            <p class="content_p">
+              布奇是以提花面料产品为主的自主研发和生产销售的综合性品牌。布奇品牌追求的是具有丰富的艺术底蕴，开放、创新的设计，沉淀出细腻的质感与美感，倡导生活走进另一种大气奢华、优雅兼容并蓄的高级姿态。
+              <br/>
+              Buqi is based on jacquard fabric products based independent research and development and production and marketing of integrated brand. Butch brand is the pursuit of rich artistic heritage, open, innovative design, exquisite texture and beauty of precipitation, advocate the attitude of life into another senior atmosphere of luxury and elegant fully inclusive and equitable.
+            </p>
+          </div>
+        </div>
+      </section>
+      <!-- index_case -->
+      <section class="index_case">
+        <div class="tem_div">
+          <div class="case_content">
+          <div class="case_left">
+            <h2 class="title1">
+              <p>案例分享</p>
+              <span>Case sharing</span>
+            </h2>
+          </div>
+          <div class="case_right"></div>
+        </div>
+      </section>
+      <!-- index_product -->
+      <section class="index_product">
+
+      </section>
+      <!-- index_design -->
+      <section class="index_design">
+
+      </section>
+
+      <?php require_once "./footer.php";?>
+    </div>
+</body>
+</html>  
