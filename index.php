f6f61c60562f5d982ef5d440dcb990ca0636984f
1075283061 <1075283061@qq.com>
2017-09-04 02:33:14 +0800

    buqi

--- index.php
+++ index.php
@@ -5,18 +5,23 @@
       <img src="./img/index_banner1.jpg" alt="index_banner">
     </div>
     <!-- index_logo -->
-    <div class="index_logo">
-      <div class="top_table">
-        <div class="top_logo">
-          <img src="./img/index_logo.png" alt="logo">
+    <div class="logo_div">
+      <div class="visibility">
+        <img src="./img/index_banner1.jpg" alt="visibility">
+      </div>
+      <div class="index_logo">
+        <div class="top_table">
+          <div class="top_logo">
+            <img src="./img/index_logo.png" alt="logo">
+          </div>
         </div>
       </div>
     </div>
      <!-- index_brand -->
-    <div class="index_body">
-      <div class="visibility">
+    <div class="visibility">
         <img src="./img/index_banner1.jpg" alt="visibility">
-      </div>
+    </div>
+    <div class="index_body">
       <?php require_once "./header.php";?>
       <!-- index_brand -->
       <section class="index_brand">
@@ -53,20 +58,45 @@
               <a href="" class="case_more">MORE+</a>
             </div> 
             <div class="case_right">
-              <div class="case_box"></div>
-              <a href="" class="arrow_left">
-                <img src="" alt="">
-              </a>
-              <a href="" class="arrow_right">
-                <img src="" alt="">
-              </a>
+              <div class="case_box">
+                <div class="case_main">
+                  <img src="./img/case.jpg" alt="case" class="case_img">
+                  <div class="case_arrow">
+                    <div class="arrow_left">
+                    </div>
+                    <div class="arrow_right">
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
       <!-- index_product -->
       <section class="index_product">
-
+        <div class="tem_div">
+          <div class="product_content">
+            <div class="product_table">
+              <div class="product_left">
+                <img src="./img/product.png" alt="product">
+              </div>
+              <div class="product_right">
+                <div class="product_main">
+                  <div class="main_p">
+                    <p>布奇窗帘做工精致，安装精细，在设计、生产、制作、安装等全过程中都重视细节，为您的家居增添一份高贵，呈现主人非凡的艺术品位，柔滑的触感、精美的细节、多样的风格，布奇绝对是营造温馨家居的不二之选。
+                    </p>
+                  </div>
+                  <div class="product_xian"></div>
+                  <h2 class="title2">
+                    <p>产品细节</p>
+                    <span>Product details</span>
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
       <!-- index_design -->
       <section class="index_design">
